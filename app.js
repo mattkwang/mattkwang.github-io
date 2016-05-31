0417394b3e6105015cd104c0040e70d03b2d01c7
Matt Wang <mattwang1997@gmail.com>
2016-05-30 19:58:04 -0700

    Recommit

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    // $('#img').css('opacity','0').load(function() {
+    //     $(this).animate({
+    //         opacity: 1
+    //     }, 500);
+    // });
+	
 	$('.overlay').click(function() {
 		$('html, body').animate({
 			scrollTop: 0
